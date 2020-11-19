004119d35592ca00b4dcd291a45bea37c60197e3
dmdwls5780 <74648747+dmdwls5780@users.noreply.github.com>
2020-11-19 09:54:53 +0900

    Add files via upload
    asdfasdf

---
+++ 수동 빌드.sql
@@ -0,0 +1,4 @@
+﻿SELECT sp_system_put_build_action(3, '', '', 'C:\NC\WRF\wrfdm1_20201101_20201103.nc', 0, 0, 0, '', False, -1, '2020-11-04 14:25:00', 1, 1);
+SELECT sp_system_put_build_action(3, '', '', 'C:\NC\WRF\wrfdm1_20201102_20201104.nc', 0, 0, 0, '', False, -1, '2020-11-04 14:25:00', 1, 1);
+SELECT sp_system_put_build_action(14, '', '', 'C:\NC\L4_OC\L4_OC_2020110112_TCSMap_format.nc', 0, 0, 0, '', False, -1, '2020-11-04 14:25:00', 1, 1);
+SELECT sp_system_put_build_action(14, '', '', 'C:\NC\L4_OC\L4_OC_2020110212_TCSMap_format.nc', 0, 0, 0, '', False, -1, '2020-11-04 14:25:00', 1, 1);
